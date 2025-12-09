5c29aac8efeb3f283a275fa6a57f752884bd4018
ivan <ivanfranco4382@gmail.com>
2025-12-09 13:40:56 -0300

    arreglando la clase comentarios

--- repositorioecommerce/ecommerce/apps/blog/models.py
+++ repositorioecommerce/ecommerce/apps/blog/models.py
@@ -23,8 +23,9 @@ class Articulo(models.Model):
 class Comentario(models.Model):
     contenido = models.TextField()
     fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE, related_name="comentarios")
 
     def __str__(self):
-        return {f"Comentario de: {self.autor.username}"}
+        return f"Comentario de: {self.autor.username}"
